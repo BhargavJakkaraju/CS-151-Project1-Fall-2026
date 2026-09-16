96641b98ccdb73023ab46959450bcdd585fb33a8
sharif-alqaabi <yashalqaabi@gmail.com>
2026-09-15 22:35:26 -0700

    Converted both test files to JUnit asserts, added more tests to each file, and added the JUnit jar

--- test/IntegerToRomanTest.java
+++ test/IntegerToRomanTest.java
@@ -1,15 +1,34 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import Roman.Solution;
 
 /*
 Tester: Sharif
 */
 
-class IntegerToRomanTest {
+public class IntegerToRomanTest {
+
     public static void main(String[] args) {
+        Test.run();
+        System.out.println("All tests passed");
+    }
+
+    private static class Test {
+
         // Sharif
-        String res = new Solution().intToRoman(3);
-        System.out.println(res.equals("III"));
+        static void run() {
+            Solution s = new Solution();
+
+            assertEquals("III", s.intToRoman(3));
+            assertEquals("LVIII", s.intToRoman(58));
+            assertEquals("MCMXCIV", s.intToRoman(1994));
+            assertEquals("I", s.intToRoman(1));
+            assertEquals("MMMCMXCIX", s.intToRoman(3999));
+            assertEquals("IV", s.intToRoman(4));
+            assertEquals("IX", s.intToRoman(9));
+            assertEquals("M", s.intToRoman(1000));
+        }
     }
 }
--- test/LongestConsecutiveSequenceTest.java
+++ test/LongestConsecutiveSequenceTest.java
@@ -1,16 +1,34 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import sequence.LongestConsecutiveSequence;
 
 /*
 Tester: Sharif
 */
 
-class LongestConsecutiveSequenceTest {
+public class LongestConsecutiveSequenceTest {
+
     public static void main(String[] args) {
+        Test.run();
+        System.out.println("All tests passed");
+    }
+
+    private static class Test {
+
         // Sharif
-        int[] nums = {1, 2, 3, 4};
-        int res = new LongestConsecutiveSequence().longestConsecutive(nums);
-        System.out.println(res == 4);
+        static void run() {
+            LongestConsecutiveSequence s = new LongestConsecutiveSequence();
+
+            assertEquals(4, s.longestConsecutive(new int[]{1, 2, 3, 4}));
+            assertEquals(4, s.longestConsecutive(new int[]{100, 4, 200, 1, 3, 2}));
+            assertEquals(9, s.longestConsecutive(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
+            assertEquals(0, s.longestConsecutive(new int[]{}));
+            assertEquals(1, s.longestConsecutive(new int[]{5}));
+            assertEquals(1, s.longestConsecutive(new int[]{1, 1, 1}));
+            assertEquals(1, s.longestConsecutive(new int[]{10, 30, 20}));
+            assertEquals(4, s.longestConsecutive(new int[]{-2, -1, 0, 1}));
+        }
     }
 }

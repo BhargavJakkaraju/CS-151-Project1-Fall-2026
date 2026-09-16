fc116bfe827c8d17153b165f6f2d068a81e2eb2a
sharif-alqaabi <yashalqaabi@gmail.com>
2026-09-15 19:05:28 -0700

    Added a test case for Bao's  solution

---
+++ test/IntegerToRomanTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import Roman.Solution;
+
+/*
+Tester: Sharif
+*/
+
+class IntegerToRomanTest {
+    public static void main(String[] args) {
+        // Sharif
+        String res = new Solution().intToRoman(3);
+        System.out.println(res.equals("III"));
+    }
+}
